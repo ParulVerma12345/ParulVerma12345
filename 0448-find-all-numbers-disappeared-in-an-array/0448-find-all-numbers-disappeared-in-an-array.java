7af03554847fd79b82a711f247d0e13b36a21dae
Parul Verma <parulverma4399@gmail.com>
2026-08-01 14:30:20 +0530

    Time: 6 ms (82.85%), Space: 67.2 MB (55.03%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        // Step 1: Mark visited numbers
+        for (int i = 0; i < nums.length; i++) {
+
+            int index = Math.abs(nums[i]) - 1;
+
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+
+        // Step 2: Find positive indexes
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] > 0) {
+                ans.add(i + 1);
+            }
+        }
+
+        return ans;
+    }
+}
